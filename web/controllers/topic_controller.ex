954441092800178084a12532176c035454271ba4
xpy <cpiwarski01@gmail.com>
2020-08-10 07:30:16 -0700

    topic: Model and view

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -2,6 +2,7 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
   def new(conn, _params) do
-
+    changeset = Discuss.Topic.changeset(%Discuss.Topic{}, %{})
+    render conn, "new.html"
   end
 end
