93ec1a1ffa2c057af15331eebacec1d3bbfa65b3
xpy <cpiwarski01@gmail.com>
2020-08-09 20:35:55 -0700

    topics: DB migration and controller

--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,9 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
+    # GET routes
     get "/", PageController, :index
+    get "/topics/new", TopicController, :new
   end
 
   # Other scopes may use custom stacks.
