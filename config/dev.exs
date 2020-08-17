4d028b8c9103e46953949b24937ca8c947defec1
evilmurries <cpiwarski00@gmail.com>
2020-08-17 06:58:21 -0700

    topics: Redirect on creation

--- config/dev.exs
+++ config/dev.exs
@@ -36,7 +36,7 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "xpy",
+  username: "christopherpiwarski",
   password: "",
   database: "discuss_dev",
   hostname: "localhost",
